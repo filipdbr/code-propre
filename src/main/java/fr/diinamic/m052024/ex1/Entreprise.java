4d35a4cea883d46702902614bdd1a1aec6bf7c1e
Filip <filip.a.dabrowski@gmail.com>
2024-08-02 14:24:02 +0200

    [TP1] Finished

---
+++ src/main/java/fr/diinamic/m052024/ex1/Entreprise.java
@@ -0,0 +1,41 @@
+package fr.diinamic.m052024.ex1;
+
+import java.util.Date;
+
+/**
+ * Représente une entreprise avec des informations de base comme le siret,
+ * le nom, l'adresse et la date de création.
+ */
+
+public class Entreprise {
+
+	/** Map de variables */
+
+	/** Numéro SIRET de l'entreprise */
+	public int siret;
+
+	/** Nom de l'entreprise */
+	public String nom;
+
+	/** Adresse de l'entreprise */
+	public String adresse;
+
+	/** Date de création de l'entreprise */
+	public Date dateCreation;
+
+	/** Capital maximum autorisé pour l'entreprise */
+	public static final int CAPITAL_MAX = 3000000;
+
+	/** Méthodes */
+
+	/**
+	 * Affiche le statut de l'entreprise.
+	 */
+	public void afficherStatut() {
+		// Méthode à implémenter
+	}
+
+	// + getters, setters et toString
+
+	}
+}
