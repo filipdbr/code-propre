2c5947a361cc79291644ee320199a4a0b845fc8a
Filip <filip.a.dabrowski@gmail.com>
2024-08-08 12:31:26 +0200

    [Update

--- src/main/java/fr/diginamic/m052024/ex3/Zoo.java
+++ src/main/java/fr/diginamic/m052024/ex3/Zoo.java
@@ -6,14 +6,25 @@ import fr.diginamic.m052024.ex3.zones.Zone;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Classe représentant un Zoo.
+ */
 public class Zoo {
 
 	// attributs
+	/** Nom du zoo */
 	private String nom;
+	/** Ensemble des animaux présents dans le zoo */
 	private Set<Animal> animals;
+	/** Ensemble des zones présentes dans le zoo */
 	private Set<Zone> zones;
 
 	// constructeur
+
+	/**
+	 * Constructeur de la classe Zoo.
+	 * @param nom Nom du zoo.
+	 */
 	public Zoo(String nom){
 		this.nom = nom;
 		this.animals = new HashSet<>();
@@ -21,39 +32,37 @@ public class Zoo {
 	}
 
 	//méthodes
+
+	/**
+	 * Ajoute un animal au zoo
+	 * @param animal un animal a ajouté
+	 */
 	public void addAnimal(Animal animal){
 		this.animals.add(animal);
 	}
-	
+
+	/**
+	 * Affiche la liste d'animaux de zoo
+	 */
 	public void afficherListeAnimaux(){
 		for (Animal animal : animals){
 			System.out.println(animal);
 		}
 	}
 
-	/** Getter for nom
-	 * @return le nom
-	 */
+	/** Getters et setters */
+
+	/** Getter et setter de nom */
+
 	public String getNom() {
 		return nom;
 	}
 
-	/** Setter
-	 * @param nom the nom to set
-	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
-	// provides access to this set
-	public Set<Animal> getAnimals() {
-		return animals;
-	}
-
-	// let me setting animals set
-	public void setAnimals(Set<Animal> animals) {
-		this.animals = animals;
-	}
+	/** Getter et setter de zones */
 
 	public Set<Zone> getZones() {
 		return zones;
@@ -63,6 +72,27 @@ public class Zoo {
 		this.zones = zones;
 	}
 
+	/** Getter et setter d'animaux' */
+
+	public Set<Animal> getAnimals() {
+		return animals;
+	}
+	
+	public void setAnimals(Set<Animal> animals) {
+		this.animals = animals;
+	}
+
+	/** Ajoute une zone au zoo
+	 * @param zone one nouvelle zone a ajouter
+	 */
+	public void addZone(Zone zone){
+		this.zones.add(zone);
+	}
+
+	/**
+	 * Retourne une représentation de l'objet Zoo sous forme de chaîne de caractères.
+	 * @return Une chaîne de caractères représentant le zoo.
+	 */
 	@Override
 	public String toString() {
 		return "Zoo{" + "nom='" + nom + '}';
